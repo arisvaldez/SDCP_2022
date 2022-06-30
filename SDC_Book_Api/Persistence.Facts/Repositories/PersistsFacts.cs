6f30d73c87ae8b788100cc9fc5694de3726763cc
Arismendy Polanco <samus45@hotmail.es>
2022-06-30 17:27:22 -0400

    added BookRepository implementation

---
+++ SDC_Book_Api/Persistence.Facts/Repositories/PersistsFacts.cs
@@ -0,0 +1,32 @@
+﻿using Core.Books;
+using NUnit.Framework;
+using Persistence.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Persistence.Facts.Repositories
+{
+    internal class PersistsFacts
+    {
+        [Test]
+        public void When_Persists_Throws_Nothing()
+        {
+            Book newBook = new Book("Pepe", "978-0-99-702549-1", new List<string>() { "PEPE" });
+            BookRepository bookRepository = new BookRepository();
+            Assert.That(() => bookRepository.Persist(newBook), Throws.Nothing);
+
+        }
+
+        [Test]
+        public void Increase_Lenght_When_Persists()
+        {
+            Book newBook = new Book("Pepe", "978-0-99-702549-1", new List<string>() { "PEPE" });
+            BookRepository bookRepository = new BookRepository();
+            int booksLength = BookRepository.Books.Count;
+
+            newBook.Persist(bookRepository);
+
+            Assert.That(() => BookRepository.Books.Count, Is.GreaterThan(booksLength + 1));
+        }
+    }
+}
--- SDC_Book_Api/Persistence/Repositories/BookRepository.cs
+++ SDC_Book_Api/Persistence/Repositories/BookRepository.cs
@@ -3,11 +3,25 @@ using Core.Boundaries.Persistence;
 
 namespace Persistence.Repositories
 {
-    internal class BookRepository : IBookRepository
+    public class BookRepository : IBookRepository
     {
-        public Task Persist(Book book)
+        public static List<Book> Books
         {
-            throw new NotImplementedException();
+            get{ return Books.GetRange(0, Books.Count); }
+            private set { _ = new List<Book>(); } 
+        }
+
+        public BookRepository()
+        {
+            Books.Add(new Book("negra nieves", "978-0-99-702542-1", new List<string> { "Pepe" }));
+            Books.Add(new Book("White girls", "978-0-99-702540-1", new List<string> { "Pepe" }));
+            Books.Add(new Book("pedro pica piedra", "978-0-99-702449-1", new List<string> { "Pepe" }));
+            Books.Add(new Book("i-c-k-k-c-k", "977-0-99-702549-1", new List<string> { "Pepe" }));
+            Books.Add(new Book("pedro suape", "979-0-99-702549-1", new List<string> { "Pepe" }));
+        }
+        public void Persist(Book book)
+        {
+            Books.Add(book);
         }
     }
 }
